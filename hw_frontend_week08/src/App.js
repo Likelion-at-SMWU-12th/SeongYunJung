6ec4a1b38fc1d83ac23731954fddbdb7f2712d82
ynzung <syj91105@sookmyung.ac.kr>
2024-09-05 22:42:44 +0900

    [Chore] title에 part 변화 적용(#39)

--- hw_frontend_week08/src/App.js
+++ hw_frontend_week08/src/App.js
@@ -1,18 +1,11 @@
-import { useState } from "react";
 import Content from "./components/Content";
 import { styled } from "styled-components";
 import { PartProvider } from "./contexts/PartContext";
-// import { usePart } from "./contexts/PartContext";
 
 function App() {
-  // const [part, setPart] = useState("");
-  // const { part, clickPart } = usePart();
   return (
     <PartProvider>
       <Wrapper>
-        <div className="title">
-          숙명여대 멋쟁이사자처럼 <span></span> 파트 구성원
-        </div>
         <Content />
       </Wrapper>
     </PartProvider>
@@ -24,13 +17,4 @@ export default App;
 const Wrapper = styled.div`
   width: 100vw;
   min-height: 100vh;
-  .title {
-    text-align: center;
-    padding: 50px 0;
-    font-size: 30px;
-    font-weight: 700;
-    span {
-      color: #ee7521;
-    }
-  }
 `;
--- hw_frontend_week08/src/components/Content.js
+++ hw_frontend_week08/src/components/Content.js
@@ -2,10 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import Left from "./Left";
 import Right from "./Right";
+import { usePart } from "../contexts/PartContext";
 
 const Content = () => {
+  const { part, clickPart } = usePart();
   return (
     <Wrapper>
+      <div className="title">
+        숙명여대 멋쟁이사자처럼 <span>{part}</span> 파트 구성원
+      </div>
       <div className="content">
         <Left />
         <Right />
@@ -19,6 +24,15 @@ export default Content;
 const Wrapper = styled.div`
   width: 100vw;
   height: calc(100vh - 130px);
+  .title {
+    text-align: center;
+    padding: 50px 0;
+    font-size: 30px;
+    font-weight: 700;
+    span {
+      color: #ee7521;
+    }
+  }
   .content {
     display: flex;
     height: 100%;
