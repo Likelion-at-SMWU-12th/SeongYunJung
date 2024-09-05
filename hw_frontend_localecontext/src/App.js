b62bae4dfab0685f1ae9889c29a089a00d28cbd1
ynzung <syj91105@sookmyung.ac.kr>
2024-09-06 00:21:10 +0900

    [Feat] context 적용(#41)

--- hw_frontend_localecontext/src/App.js
+++ hw_frontend_localecontext/src/App.js
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router";
 import ReviewPage from "./pages/ReviewPage";
 import CreatePage from "./pages/CreatePage";
 import ToggleBtn from "./components/ToggleBtn";
+import ThemeContext from "./contexts/ThemeContext";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<ReviewPage />} />
-        <Route path="/create" element={<CreatePage />} />
-      </Routes>
-      <ToggleBtn />
+      <ThemeContext.Provider value={"light"}>
+        <Routes>
+          <Route path="/" element={<ReviewPage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+        <ToggleBtn />
+      </ThemeContext.Provider>
     </>
   );
 }
