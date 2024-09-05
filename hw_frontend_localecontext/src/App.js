4783cff8c4f744d8227334994aa046667104d892
ynzung <syj91105@sookmyung.ac.kr>
2024-09-06 00:18:27 +0900

    [Init] 파일구조 세팅(#41)

--- hw_frontend_localecontext/src/App.js
+++ hw_frontend_localecontext/src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
+import ReviewPage from "./pages/ReviewPage";
+import CreatePage from "./pages/CreatePage";
+import ToggleBtn from "./components/ToggleBtn";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<ReviewPage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+      <ToggleBtn />
+    </>
+  );
 }
 
 export default App;
