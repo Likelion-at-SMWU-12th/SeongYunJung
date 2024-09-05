6829bc3ac5270f405457e98711861c54bef7a5d3
ynzung <syj91105@sookmyung.ac.kr>
2024-09-06 00:26:13 +0900

    [Feat] Provider 컴포넌트와 커스텀 훅 생성(#41)

--- hw_frontend_localecontext/src/contexts/ThemeContext.js
+++ hw_frontend_localecontext/src/contexts/ThemeContext.js
@@ -1,5 +1,18 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const ThemeContext = createContext();
 
-export default ThemeContext;
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    console.log("테마 변경: ", theme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
