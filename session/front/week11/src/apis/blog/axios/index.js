990a0053dc150a0232fa6e646a3238c7a5b09e22
ynzung <syj91105@sookmyung.ac.kr>
2024-09-30 15:18:07 +0900

    [Add] 프론트엔드 11주차 과제

--- session/front/week11/src/apis/blog/axios/index.js
+++ session/front/week11/src/apis/blog/axios/index.js
@@ -19,3 +19,24 @@ export const deletePost = async (postId) => {
   const { data } = await axios.delete(`api/posts/${postId}`);
   return data;
 };
+
+// 프론트 11주차 과제
+export const postSignup = async (username, password) => {
+  const { data } = await axios.post(`/api/signup`, username, password);
+  return data;
+};
+
+export const updateProfile = async (username) => {
+  const { data } = await axios.put(`/api/editInfo`, username);
+  return data;
+};
+
+export const myPageFetch = async (userId) => {
+  const { data } = await axios.get(`api/myPageFetch/${userId}`);
+  return data;
+};
+
+export const deleteUser = async (userId) => {
+  const { data } = await axios.delete(`api/delete/${userId}`);
+  return data;
+};
